9e345db2389f0821ffb665cb43d41ad2523f2a8c
Gonzao Ortega <ortega_gonzalo@hotmail.com>
2019-04-09 19:54:59 -0300

    tercer commit

--- la_segunda/settings.py
+++ la_segunda/settings.py
@@ -40,27 +40,20 @@ INSTALLED_APPS = [
 
 
     'rest_framework',
-    'rest_framework.authtoken',
 
     'snippets.apps.SnippetsConfig',
 
-    'djoser',
 ]
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+
     'DEFAULT_PAGINATION_CLASS': 
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
 
-SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
-}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- snippets/urls.py
+++ snippets/urls.py
@@ -2,8 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from snippets import views
 
-from django.conf.urls import url
-
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
@@ -12,9 +10,4 @@ router.register(r'users', views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('', include(router.urls)),
-    url(r'^auth/', include('djoser.urls')),
-    #path('auth/', include('djoser.urls')),
-    url(r'^auth/', include('djoser.urls.authtoken')),
-
-    url(r'^auth/', include('djoser.urls.jwt')),
 ]
